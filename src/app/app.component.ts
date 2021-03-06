126bae88cc63795b95bff4c72e2d27c48159ee9e
Péter Szabó <peter.szabo@infinitect.hu>
2021-03-06 17:42:53 +0100

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+
+import moment from "moment";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = 'day-counter';
+  title = "day-counter";
+  counterValue_ = moment().startOf("month");
+  counterValue__ = moment().to(moment("20210315"));
+  counterValue = moment("20210315").fromNow();
 }
